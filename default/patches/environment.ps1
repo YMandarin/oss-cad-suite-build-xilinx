1caab788c6369cf81e75fcbb44722839e94b8163
Miodrag Milanovic <mmicko@gmail.com>
2026-09-14 08:48:20 +0200

    windows: Fix for spaces in path

--- default/patches/environment.ps1
+++ default/patches/environment.ps1
@@ -1,6 +1,6 @@
 # Find root dir
 if (-not $env:YOSYSHQ_ROOT) {
-    $env:YOSYSHQ_ROOT = Join-Path (Get-Item -Path $PSScriptRoot).FullName "\"
+    $env:YOSYSHQ_ROOT = (Get-Item -LiteralPath $PSScriptRoot).FullName
 }
 
 # Set prompt
@@ -10,7 +10,7 @@ function Prompt {
 
 # Set environment variables
 $env:SSL_CERT_FILE = Join-Path $env:YOSYSHQ_ROOT "etc\cacert.pem"
-$env:PATH = "$($env:YOSYSHQ_ROOT)bin;$($env:YOSYSHQ_ROOT)lib;$env:PATH"
+$env:PATH = @((Join-Path $env:YOSYSHQ_ROOT "bin"), (Join-Path $env:YOSYSHQ_ROOT "lib"), $env:PATH) -join ";"
 $env:PYTHON_EXECUTABLE = Join-Path $env:YOSYSHQ_ROOT "lib\python3.exe"
 $env:QT_PLUGIN_PATH = Join-Path $env:YOSYSHQ_ROOT "lib\qt6\plugins"
 $env:QT_LOGGING_RULES = "*=false"
@@ -20,7 +20,7 @@ $env:GDK_PIXBUF_MODULEDIR = Join-Path $env:YOSYSHQ_ROOT "lib\gdk-pixbuf-2.0\2.10
 $env:GDK_PIXBUF_MODULE_FILE = Join-Path $env:YOSYSHQ_ROOT "lib\gdk-pixbuf-2.0\2.10.0\loaders.cache"
 
 # Update GDK pixbuf loaders cache
-& "$env:YOSYSHQ_ROOT\lib\gdk-pixbuf-query-loaders.exe" --update-cache
+& (Join-Path $env:YOSYSHQ_ROOT "lib\gdk-pixbuf-query-loaders.exe") --update-cache
 
 # Additional environment variable
 $env:OPENFPGALOADER_SOJ_DIR = Join-Path $env:YOSYSHQ_ROOT "share\openFPGALoader"
